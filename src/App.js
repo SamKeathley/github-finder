9b37a0122e1a9137396d6f984b70e07cf7667830
Sam Keathley <samncollette@gmail.com>
2020-10-28 14:36:20 -0700

    start setup for navbar and users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Hello... Is it me you're looking for?</h1>
-    </div>
-  );
+class App extends Component{
+
+  render() {
+    return (
+      <div className="App">
+      <Navbar />
+      <UserItem />
+      </div>
+    );
+  }
 }
 
 export default App;
